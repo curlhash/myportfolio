861d0a78c1b8d129871826628ee2c9edfb781a85
abhishek verma <abhishekverma@testbook.com>
2017-09-10 05:37:26 +0530

    blinking eye

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.sendFile(__dirname + '/dist/');
 });
 
+app.get('/:name', function (req, res) {
+    res.sendFile(__dirname + '/dist/' + req.params.name);
+});
+
+
 app.listen(80, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('App listening on port 80!');
 });
