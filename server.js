7346c5e88f1767d6019be5d663c4bf55b983d6ce
abhishek verma <abhishekverma@testbook.com>
2018-02-05 01:31:56 +0530

    removed dependency on testflutter

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/dist/');
 });
-
-app.get('/:name', function (req, res) {
-    res.sendFile(__dirname + '/dist/' + req.params.name);
-});
-
 
 app.listen(80, function () {
     console.log('App listening on port 80!');
