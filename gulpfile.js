82f35c064272006ff2a930c4a0bee229317225fc
abhishek verma <abhishekverma@testbook.com>
2018-11-21 18:37:51 +0530

    explosion init

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var path = require("path");
 var runSequence = require('run-sequence');
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
 
 var srcAllFiles = path.join('src', '**', '*');
+var srcAllJsFiles = path.join('src', 'assets', 'js', '*');
 var dist = path.join('dist');
 
 gulp.task('clean', function () {
@@ -11,13 +14,29 @@ gulp.task('clean', function () {
 });
 
 gulp.task('build', function(){
+	var JS_FILTER = filter(srcAllJsFiles, {restore: true});
 	return gulp.src(srcAllFiles)
+		.pipe(JS_FILTER)
+		.pipe(uglify())
+		.pipe(JS_FILTER.restore)
 		.pipe(gulp.dest(dist));
 });
 
-gulp.task('build-site', function(callback){
+gulp.task('watch', function() {
+	gulp.watch( srcAllFiles, { interval: 1000, usePolling: true }, ['build']);
+});
+
+gulp.task('prod', function(callback){
 	return runSequence(
 		'clean',
 		'build',
+		callback);
+});
+
+gulp.task('dev', function(callback){
+	return runSequence(
+		'clean',
+		'build',
+		'watch',
 		callback);
 });
--- src/assets/js/particle.js
+++ src/assets/js/particle.js
@@ -6,20 +6,6 @@
 			};
 	})();
 
-	function initParticles(num) {
-		var returnArr = [];
-		var particleObj = {};
-		for (var i = 0; i < num; i++) {
-			particleObj = {
-				x: canvas.width/2,
-				y: canvas.height/2,
-				radius: 8,
-				opacity: 1
-			};
-			returnArr.push(particleObj)
-		}
-		return returnArr;
-	}
 	function drawParticle(arr, context) {
 		var colorRGB;
 		colorRGB = colorPallete[Math.floor(Math.random() * colorPallete.length)] || '0,255,56';
@@ -37,6 +23,30 @@
 		var g = parseInt(hex.substring(2, 4), 16);
 		var b = parseInt(hex.substring(4, 6), 16);
 		return r + ', ' + g + ', ' + b;
+	}
+	function generateColorPallete (hexArr) {
+		var returnArr = [];
+		for (var i = 0; i < hexArr.length; i++) {
+			returnArr.push(convertHexColor(hexArr[i]));
+		}
+		return returnArr;
+	}
+
+	function initParticles(num, radius, opacity, isRandom) {
+		var returnArr = [];
+		var particleObj = {};
+		radius = isRandom ? Math.floor(Math.random() * radius) + 1 : radius;
+		opacity = isRandom ? Math.floor(Math.random() * opacity) : opacity;
+		for (var i = 0; i < num; i++) {
+			particleObj = {
+				x: canvas.width/2,
+				y: canvas.height/2,
+				radius: radius || 10,
+				opacity: opacity || 1
+			};
+			returnArr.push(particleObj)
+		}
+		return returnArr;
 	}
 	function animate(arr, canvas, context, startTime) {
 		// update
@@ -66,22 +76,90 @@
 		});
 	}
 
-	var colorPallete = [convertHexColor('009ab0'), convertHexColor('82efee'), convertHexColor('cfbca6'), convertHexColor('128a08')];
+	function initExplosionParticles(num, radius, opacity, isRandom) {
+		var theta = 0;
+		var returnArr = [];
+		var particleObj = {};
+		var circleRadius = canvas.width/50;
+		var centerX = canvas.width / 2;
+		var centerY = canvas.height / 2;
+		radius = isRandom ? Math.floor(Math.random() * radius) + 1 : radius;
+		opacity = isRandom ? Math.floor(Math.random() * opacity) : opacity;
+		for (var i = 0; i < num; i++) {
+			particleObj = {
+				x: circleRadius * Math.cos(theta) + centerX,
+				y: circleRadius * Math.sin(theta) + centerY,
+				radius: radius || 10,
+				opacity: opacity || 1,
+				theta: theta
+			};
+			returnArr.push(particleObj);
+			theta += 2 * Math.PI / num;
+		}
+		return returnArr;
+	}
+	function explosionAnimate(arr, canvas, context, startTime) {
+		// update
+		for(var i = 0; i < arr.length; i++) {
+			var time = (new Date()).getTime() - startTime + 1;
+			var speed = 0.01;
+			var distance = time * speed;
+			var nextX = distance * Math.cos(arr[i].theta) + arr[i].x;
+			var nextY = distance * Math.sin(arr[i].theta) + arr[i].y;
+			arr[i].x = nextX;
+			arr[i].y = nextY;
+			arr[i].radius -= 0.12;
+		}
+
+		context.fillStyle = 'rgba(0, 0, 0, 0.01)';
+		context.fillRect(0, 0, canvas.width, canvas.height);
+
+		// draw
+		drawParticle(arr, context);
+
+		// request new frame
+		requestAnimFrame(function() {
+			explosionAnimate(arr, canvas, context, startTime);
+		});
+	}
+
+	function setAnimation (callback, particlesArr, canvas, context) {
+		// wait one second before starting animation
+		setTimeout(function() {
+			var startTime = (new Date()).getTime();
+			callback(particlesArr, canvas, context, startTime);
+		}, 1000);
+	}
+
+	// random selection of the experiment
+	var EXPERIMENTS = {
+		spiral: 'SPIRAL',
+		explosion: 'EXPLOSION'
+	};
+	var DECIDING_ARR = ['SPIRAL', 'EXPLOSION'];
+	var EXPERIMENT_COUNT = 2;
+
+	var colorPallete = generateColorPallete(['009ab0', '82efee', 'cfbca6', '128a08']);
 	var canvas = document.getElementById('myCanvas');
 	var context = canvas.getContext('2d');
-	var particlesArr = initParticles(50);
-
-//drawRectangle(myRectangle, context);
+	var chance = DECIDING_ARR[Math.floor(Math.random() * EXPERIMENT_COUNT)];
+	var particleCount, particlesArr;
+	if (chance === EXPERIMENTS.spiral) {
+		particleCount = 50;
+		particlesArr = initParticles(particleCount, 5, 1, false);
+		drawParticle(particlesArr, context);
+		setAnimation(animate, particlesArr, canvas, context);
+	} else if (chance === EXPERIMENTS.explosion) {
+		particleCount = 50;
+		particlesArr = initExplosionParticles(particleCount, 10, 1, false);
+		drawParticle(particlesArr, context);
+		setAnimation(explosionAnimate, particlesArr, canvas, context);
+	} else {
+		particleCount = 50;
+		particlesArr = initParticles(particleCount);
+		drawParticle(particlesArr, context);
+		setAnimation(animate, particlesArr, canvas, context);
+	}
 	context.fillStyle = 'rgba(0, 0, 0, 0.07)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
-	drawParticle(particlesArr, context);
-
-
-
-
-// wait one second before starting animation
-	setTimeout(function() {
-		var startTime = (new Date()).getTime();
-		animate(particlesArr, canvas, context, startTime);
-	}, 1000);
 })();
